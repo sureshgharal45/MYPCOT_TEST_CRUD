70743cc101c173fdc8da7c239aafd5b27e0a8f41
sureshgharal45 <sureshgharal65@gmail.com>
2023-09-01 23:42:11 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -26,20 +26,20 @@ app.use(cookieParser());
 //user routes
 app.use("/api/v1", require("./routes/userRoutes"));
 
-// --------------------------deployment------------------------------
-const __dirname1 = path.resolve();
-console.log(__dirname1);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
+// --------------------------deployment-purpose------------------------------
+// const __dirname1 = path.resolve();
+// console.log(__dirname1);
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+// --------------------------deployment-purpose------------------------------
 
 //middleware for errors
 app.use(errorMiddleware);
